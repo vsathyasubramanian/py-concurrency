945b011e3fadc6be115b46a4857773288115adf0
sathyasubramanian <sathya.subramanian@kissflow.com>
2022-10-16 17:58:33 +0530

    all yield server added

--- all_yield_server.py
+++ all_yield_server.py
@@ -3,55 +3,38 @@ from collections import deque
 from select import select
 from socket import *
 
-from fib import fib, fib_ay
-from concurrent.futures import ProcessPoolExecutor as Pool
-pool = Pool(3)
+from fib import fib_generator
 
-result = {"Fib": "NA"}
 active_tasks = deque()
+
+result_bank = {}
 recv_wait = {}
 send_wait = {}
-future_wait = {}
-future_notify, future_event = socketpair()
+compute_wait = {}
 
-def future_done(future):
-    active_tasks.append(future_wait.pop(future))
-    future_notify.send(b'x')
-
-def future_monitor():
-    while True:
-        yield future_event, 'recv'
-        future_event.recv(100)
-
-active_tasks.append(future_monitor())
 
 def run():
-    while any([active_tasks,recv_wait, send_wait]):
+    while any([active_tasks, recv_wait, send_wait]):
         while active_tasks:
             _task = active_tasks.popleft()
             try:
-                print(_task)
                 obj, action = next(_task)
+                # IO calls socket waits
                 if action in ("accept", "recv"):
                     recv_wait[obj] = _task
                 elif action in ("send"):
                     send_wait[obj] = _task
-                elif action == 'future':
-                    future_wait[obj] = _task
-                    obj.add_done_callback(future_done)
-                elif action == "run":
-                    # need to put obj in task queue the generator
+                # Custom logic to handle compute tasks
+                elif action == 'compute':
+                    compute_wait[obj] = _task
                     active_tasks.append(obj)
-                    future_wait[obj] = _task
-                    print(f"THE OBJ FROM FUTURE {obj}")
                 elif action == "repeat":
                     # need to re-iterate the generator
                     active_tasks.append(_task)
                 elif action == "res":
-                    print(f"value is {obj}")
-                    result["Fib"] = obj
-                    print(f"THE RES FROM CO {_task}")
-                    print(f"Now really dont know what to do!!!")
+                    # Some task has given results. pop the parent task from compute wait
+                    result_bank[_task] = obj
+                    active_tasks.append(compute_wait.pop(_task))
             except StopIteration:
                 print("Done!")
         # No active tasks to run. Handle IO waits
@@ -61,25 +44,21 @@ def run():
         for _send in send:
             active_tasks.append(send_wait.pop(_send))
 
+
 class Handler:
     @staticmethod
     def fib_handler(client):
         while True:
             yield client, 'recv'
-            req = client.recv(100)      # Blocking
+            req = client.recv(100)  # Blocking
             if not req:
                 break
             n = int(req)
-            # result = "NA"
-            # result = fib(n)             # processing intensive
-            # future = pool.submit(fib, n)
-            # yield future, 'future'         # Blocking
-            # result = future.result()
-            # result = fib_ay(n)
-            yield fib_ay(n), 'run'
-            resp = str(result).encode('ascii') + b'\n'
+            _gen = fib_generator(n)  # Compute
+            yield _gen, 'compute'
+            resp = str(result_bank[_gen]).encode('ascii') + b'\n'
             yield client, 'send'
-            client.send(resp)           # Blocking
+            client.send(resp)  # Blocking
         print("Closed")
 
 
@@ -94,7 +73,7 @@ class YieldServer:
         sock.listen(5)
         while True:
             yield sock, 'accept'
-            client, addr = sock.accept()        # Blocking
+            client, addr = sock.accept()  # Blocking
             print("Connection", addr)
             active_tasks.append(self.handler_obj.fib_handler(client))
 
@@ -103,4 +82,3 @@ if __name__ == '__main__':
     fib_server = YieldServer()
     active_tasks.append(fib_server.start(('', 26000)))
     run()
-
--- fib.py
+++ fib.py
@@ -2,5 +2,17 @@ def fib(n):
     if n <= 2:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib(n - 1) + fib(n - 2)
 
+
+def _fibbonacci_generator(n, fibArray):
+    for i in range(2, n + 1):
+        yield fibArray[i - 1] + fibArray[i - 2]
+
+
+def fib_generator(n):
+    fibArray = [0, 1]
+    for v in _fibbonacci_generator(n, fibArray):
+        fibArray.append(v)
+        yield '', 'repeat'
+    yield fibArray[n], 'res'
